4514e9c53674e5e79506e7beb1c871f09f2abe63
Utku Çetinkaya <utku.cetinkaya@hotmail.com>
2022-10-31 14:19:01 +0300

    Created observable class for binding

---
+++ MovieListMVVM/Classes/Observable.swift
@@ -0,0 +1,30 @@
+//
+//  Observable.swift
+//  MovieListMVVM
+//
+//  Created by Utku Çetinkaya on 31.10.2022.
+//
+
+import Foundation
+
+class Observable<T> {
+    
+    var value: T? {
+        didSet {
+            DispatchQueue.main.async {
+                self.listener?(self.value)
+            }
+        }
+    }
+    
+    init( _ value: T?) {
+        self.value = value
+    }
+    
+    private var listener: ((T?) -> Void)?
+    
+    func bind( _ listener: @escaping ((T?) -> Void)) {
+        listener(value)
+        self.listener = listener
+    }
+}
